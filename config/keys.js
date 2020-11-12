1a5fbb037f36f8eb2ae5326165dfd7575a5d7498
ShaiPreter <shai.preter@gmail.com>
2020-11-12 18:44:41 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://Shai:Shis1996@turnt-j1q2b.mongodb.net/EECS-4314?retryWrites=true&w=majority';
+dbPassword = 'Put your mongodb password url here';
 
 module.exports = {
     mongoURI: dbPassword
